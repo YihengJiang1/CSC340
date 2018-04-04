fb16cbe6bb78f770daf89eef6fa93babde425657
YihengJiang1 <38018793+YihengJiang1@users.noreply.github.com>
2018-04-04 18:22:14 -0400

    Add files via upload

---
+++ EmployeeView.java
@@ -0,0 +1,32 @@
+package Manager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yiheng JIang
+ */
+public class EmployeeView {
+
+
+   public void printCustomerinformation(String Name, int ID,String town ,String street,String Password,String Postcode,String Username){
+      
+     
+      System.out.println("Customer information"+": ");
+      System.out.println("Name: " + Name);
+      System.out.println("ID Number: " + ID);
+      System.out.println("Street: "+street);
+      System.out.println("Town: "+town);
+      System.out.println("Username: "+Username);
+      System.out.println("Password: "+Password);
+      System.out.println("Postcode: "+Postcode);
+      
+     
+      }
+}
+    
+
